90ab7b057bdfa74fac4cb9b29a9b28673ddeef10
palgd <mememeron21@gmail.com>
2014-10-27 19:41:56 +0900

    booksテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027094254) do
+ActiveRecord::Schema.define(version: 20141027102045) do
+
+  create_table "books", force: true do |t|
+    t.string  "name",         null: false
+    t.integer "author_id"
+    t.integer "publisher_id"
+    t.integer "category_id"
+    t.integer "price"
+  end
 
   create_table "users", force: true do |t|
     t.string "name",               null: false
