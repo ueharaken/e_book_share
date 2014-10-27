f70e56444ba53a7237a54a75a670623a52a76595
NaoyaYamamoto <ponta@pankaku.com>
2014-10-27 15:30:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EBookShare::Application.config.secret_key_base = '9d8d6c0f47f40a2312a00eebfbf8d4951c821bc47b9ef3b752fc366aa45264e8cbf1c20e9a9351d19b3604e2063d4e68313684ea0dfa742c1c0a57e90ec991d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EBookShare::Application.config.session_store :cookie_store, key: '_e_book_share_session'
