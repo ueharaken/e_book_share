8519ed3cf5ad24f9959e43c38dea241fd49783fd
palgd <mememeron21@gmail.com>
2014-11-17 15:37:13 +0900

    bookテーブルへのデータ挿入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,22 @@ class BooksController < ApplicationController
 
   def show
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to '/books/'
+    else
+      render '/books/new'
+    end
+  end
+
+  private
+  def book_params
+    params.require(:@book).permit(:name, :author, :publisher, :category_id, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 EBookShare::Application.routes.draw do
   resources :books
-  #resources :books
+  resources :users, only: %i(index new create)
+  post "books/new" => "books#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
