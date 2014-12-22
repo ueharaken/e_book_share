3eb5295161cb04a77353ff1d27e48c59f3623748
NaoyaYamamoto <ponta@pankaku.com>
2014-12-22 13:58:37 +0900

    ransackを使って検索フォームを作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,38 +1,3 @@
 class Book < ActiveRecord::Base
-  scope :by_name, lambda {|name|
-    if name.present?
-      tokens = name.split(' ').map { |c| "%#{c}%" }
-      if tokens.present?
-        where((['( name like ? )']*tokens.size).join(' AND '),
-          *tokens.map { |token| [token] * 1 }.flatten)
-      end
-    else
-      {}
-    end
-  }
-
-  scope :by_author, lambda {|author|
-    if author.present?
-      tokens = author.split(' ').map { |c| "%#{c}%" }
-      if tokens.present?
-        where((['( author like ? )']*tokens.size).join(' AND '),
-          *tokens.map { |token| [token] * 1 }.flatten)
-      end
-    else
-      {}
-    end
-  }
-
-  scope :by_publisher, lambda {|publisher|
-    if publisher.present?
-      tokens = publisher.split(' ').map { |c| "%#{c}%" }
-      if tokens.present?
-        where((['( publisher like ? )']*tokens.size).join(' AND '),
-          *tokens.map { |token| [token] * 1 }.flatten)
-      end
-    else
-      {}
-    end
-  }
 
 end
